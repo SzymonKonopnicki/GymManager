2394b9ae73e604c20185761677b6065e5c95877c
Czarno Mysz <szymonkonopnicki@outlook.com>
2024-04-24 21:00:46 +0200

    Globalize for future culture

--- GymManager.UI/Extensions/IServiceCollectionExtension.cs
+++ GymManager.UI/Extensions/IServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
 
 namespace GymManager.UI.Extensions;
 
@@ -19,5 +21,24 @@ public static class IServiceCollectionExtension
             x.ViewLocationFormats.Add("/Views/Basic/Shared/{0}" + RazorViewEngine.ViewExtension);
         });
 
+    }
+
+    public static void AddCulture(this IServiceCollection services)
+    {
+        var supportCultures = new List<CultureInfo>
+        {
+            new CultureInfo("pl"),
+            new CultureInfo("en")
+        };
+
+        CultureInfo.DefaultThreadCurrentCulture = supportCultures[0];
+        CultureInfo.DefaultThreadCurrentUICulture = supportCultures[0];
+
+        services.Configure<RequestLocalizationOptions>(opt =>
+        {
+            opt.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(supportCultures[0]);
+            opt.SupportedCultures = supportCultures;
+            opt.SupportedUICultures = supportCultures;
+        });
     }
 }
