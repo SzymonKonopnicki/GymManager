97754b9fe89a08622a87c6c1d9cc0b21ef2a82ee
Czarno Mysz <szymonkonopnicki@outlook.com>
2024-05-06 23:25:36 +0200

    Domain model

--- GymManager.Infrastructure/DependencyInjection.cs
+++ GymManager.Infrastructure/DependencyInjection.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using GymManager.Application.Common.Interfaces;
+using GymManager.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GymManager.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection service)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
     {
+        service.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefualtConnection");
 
+        service.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            opt.UseSqlServer(connectionString)
+            .EnableSensitiveDataLogging();
+        });
         return service;
     }
 
